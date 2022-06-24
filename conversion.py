4f5e89b49aaa4b98db7cf6522952ef41bd9af11e
Cgenios <creagenioshn01@gmail.com>
2022-06-23 19:25:25 -0600

    construccion del menu del programa 1er intento

---
+++ conversion.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 19 22:26:04 2022
+
+@author: Luna
+"""
+"""
+Estructura basica, menu del programa
+"""
+
+def menu():
+    print("Seleccione una opción que tipo de conversión necesita")
+    print("               1.DD a DMS")
+    print("               2.DMS a DD")
+
+menu()
