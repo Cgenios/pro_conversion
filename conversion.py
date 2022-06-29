e6a3843c79b0210fde33b122653f22d791ebab98
Cgenios <creagenioshn01@gmail.com>
2022-06-28 20:03:07 -0600

    Código completo para la conversión de coordenadas

--- conversion.py
+++ conversion.py
@@ -10,8 +10,8 @@ Estructura basica, menu del programa
 #menu y contenedores básicos
 def menu():
     print("Seleccione una opción que tipo de conversión necesita:")
-    print("               1.DD a DMS")
-    print("               2.DMS a DD")
+    print("              1.DMS a DD")
+    print("              2.DD a DMS")
     opcion=int(input())
     if opcion==1:
         #coversexa()
@@ -22,9 +22,10 @@ def menu():
     
 #peticion de datos en caso de ser sexagesimal 
 def pedir_datos_sexa():
-    datsexalat=input('Ingrese latitud en sexagesimal (grados): ')
-    datsexalon=input('Ingrese longitud en sexagesimal (grados): ')
-    grados=covern(datsexalat,datsexalon)
+    latsexa=input('Ingrese latitud en sexagesimal (grados minutos segundos): ')
+    lonsexa=input('Ingrese latitud en sexagesimal (grados minutos segundos): ')
+    sexa=covern(latsexa,lonsexa)
+    coversad(sexa)
     return 0
 
 
@@ -57,7 +58,9 @@ def coverdas(data):
     #print(conlat[1])
     res=str(conlat[1]).split(sep='.')
     seg=float("0."+res[1])*60
-    print("Los grados latitud: "+conlat[0]+"°"+" "+res[0]+"' "+str(seg)+"''")
+    print("######################################################")
+    print(" ")
+    print("Los grados latitud : "+conlat[0]+"°"+" "+res[0]+"' "+str(seg)+"''")
     
     #Tratamiento de datos en longitud
     conlon[1]=float("0."+conlon[1])*60
@@ -65,5 +68,36 @@ def coverdas(data):
     res1=str(conlon[1]).split(sep='.')
     seg1=float("0."+res1[1])*60
     print("Los grados longitud: "+conlon[0]+"°"+" "+res1[0]+"' "+str(seg1)+"''")
+    
+def coversad(data):
+    lat=data[0] #Division para la conversion de la tupla
+    lon=data[1] #División para la conversion de la tupla
+    conlat=lat.split() #Se utiliza un separador en python
+    conlon=lon.split() #Se utiliza un separador en python
+    
+    
+    #Tratamiento de latitud e impresion
+    conlat[2]=float(conlat[2])/60
+    conlat[1]=((float(conlat[1]))+conlat[2])/60
+    if float(conlat[0])<0:
+        res=float(conlat[0])-conlat[1]
+    else:
+         res=float(conlat[0])+conlat[1]
+    print("######################################################")
+    print("Latitud en decimales: ",res)
+    
+    
+    #Tratamiento de longitud e Impresion
+    #Tratamiento de latitud e impresion
+    conlon[2]=float(conlon[2])/60
+    conlon[1]=((float(conlon[1]))+conlon[2])/60
+    if float(conlon[0])<0:
+        res1=float(conlon[0])-conlon[1]
+    else:
+         res1=float(conlon[0])+conlon[1]
+    print(" ")
+    print("Longitud en decimales: ",res)
+    
+    return 0
 
 menu()
